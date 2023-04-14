c0bd8d855b57e81a698c7edfdd375500197af02f
DJ Madeira <dmadeira@medallia.com>
2023-04-14 13:55:21 -0400

    heyy progress

--- roll20/page_script.js
+++ roll20/page_script.js
@@ -3,11 +3,39 @@ console.log('Greasy Wheel: page script');
 const init = () => {
     console.log('Greasy Wheel: init');
 
-    window.Campaign.on('change:turnorder', (Campaign) => {
+    window.Campaign.on('change:turnorder', () => {
+        const characters = [];
+        const turnOrder = JSON.parse(window.Campaign.get('turnorder'));
+
+        turnOrder.forEach((turnOrderItem) => {
+            const { id, _pageid } = turnOrderItem;
+            const page = window.Campaign.pages._byId[_pageid];
+
+            if (!page) {
+                console.error('page not found', _pageid, id);
+                return;
+            }
+
+            const token = page.thegraphics._byId[id];
+
+            if (!token || !token.character) {
+                console.error('token not found', _pageid, id);
+                return;
+            }
+
+            characters.push({
+                tokenId: id,
+                characterId: token.character.id,
+                characterName: token.character.attributes.name
+            });
+        });
+
+        console.log('roll20/page_script/change:turnorder', characters);
+
         window.postMessage({
             to: 'ext',
             type: 'ROLL20_TURN_ORDER_CHANGE',
-            payload: Campaign.get('turnorder')
+            payload: characters
         }, '*');
     });
 };
